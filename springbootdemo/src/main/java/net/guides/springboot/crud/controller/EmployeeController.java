9b9d66eaeb82a15fd63a565a198250270ef7c3fc
c844675 <jumanah.al-hazba@standardbank.co.za>
2022-03-23 21:00:00 +0100

    Separate backend after cleaned installed with title column added and image mapping (RedirectView)

---
+++ springbootdemo/src/main/java/net/guides/springboot/crud/controller/EmployeeController.java
@@ -0,0 +1,147 @@
+package net.guides.springboot.crud.controller;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+//added for image
+import java.io.File;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import net.guides.springboot.crud.exception.ResourceNotFoundException;
+import net.guides.springboot.crud.model.Employee;
+import net.guides.springboot.crud.repository.EmployeeRepository;
+import net.guides.springboot.crud.service.SequenceGeneratorService;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.view.RedirectView;
+//import org.springframework.util;
+//import org.apache.commons.FileUpload.util;
+
+@RestController
+@RequestMapping("/api/v1")
+@CrossOrigin(origins = "*")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private SequenceGeneratorService sequenceGeneratorService;
+
+    @GetMapping("/login")
+    public String login(){return "Success";}
+
+    @GetMapping("/employees")
+    public List < Employee > getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    @GetMapping("/employees/{id}")
+    public ResponseEntity < Employee > getEmployeeById(@PathVariable(value = "id") Long employeeId)
+            throws ResourceNotFoundException {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+        return ResponseEntity.ok().body(employee);
+    }
+    @CrossOrigin(origins = "http://localhost:3000", maxAge = 3600)
+    @PostMapping("/employees")
+    public Employee createEmployee(@Valid @RequestBody Employee employee) {
+        System.out.println((employee));
+        employee.setId(sequenceGeneratorService.generateSequence(Employee.SEQUENCE_NAME));
+        return employeeRepository.save(employee);
+    }
+    @PostMapping("/users/save")
+    public RedirectView saveUser(Employee employee, MultipartFile multipartFile) throws IOException {
+
+        String fileName = StringUtils.cleanPath(multipartFile.getOriginalFilename());
+        employee.setTitle(fileName);
+
+        Employee savedUser = employeeRepository.save(employee);
+
+        String uploadDir = "user-photos/" + savedUser.getId();
+
+        //FileUploadUtil.saveFile(uploadDir, fileName, multipartFile);
+        Path uploadPath = Paths.get(uploadDir);
+
+        if (!Files.exists(uploadPath)) {
+            Files.createDirectories(uploadPath);
+        }
+
+        try (InputStream inputStream = multipartFile.getInputStream()) {
+            Path filePath = uploadPath.resolve(fileName);
+            Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+        } catch (IOException ioe) {
+            throw new IOException("Could not save image file: " + fileName, ioe);
+        }
+
+        return new RedirectView("/users", true);
+    }
+
+
+
+    @PutMapping("/employees/{id}")
+    public ResponseEntity < Employee > updateEmployee(@PathVariable(value = "id") Long employeeId,
+                                                      @Valid @RequestBody Employee employeeDetails) throws ResourceNotFoundException {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+
+        employee.setEmailId(employeeDetails.getEmailId());
+        employee.setLastName(employeeDetails.getLastName());
+        employee.setFirstName(employeeDetails.getFirstName());
+        employee.setTitle(employeeDetails.getTitle());
+        final Employee updatedEmployee = employeeRepository.save(employee);
+        return ResponseEntity.ok(updatedEmployee);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public Map < String, Boolean > deleteEmployee(@PathVariable(value = "id") Long employeeId)
+            throws ResourceNotFoundException {
+        Employee employee = employeeRepository.findById(employeeId).orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id :: " + employeeId));
+
+        employeeRepository.delete(employee);
+        Map < String, Boolean > response = new HashMap < > ();
+        response.put("deleted", Boolean.TRUE);
+        return response;
+    }
+//    //image add
+//    private static String imageDirectory = System.getProperty("user.dir") + "/images/";
+//    //@GetMapping(value = "/image", produces = {MediaType.IMAGE_PNG_VALUE, "application/json"})
+//    @GetMapping("/image")
+//    public String test(){return "Success";}
+//    public ResponseEntity<?> uploadImage(@RequestParam("imageFile")MultipartFile file,
+//                                         @RequestParam("imageName") String name) {
+//        makeDirectoryIfNotExist(imageDirectory);
+//        Path fileNamePath = Paths.get(imageDirectory,
+//                name.concat(".").concat(FilenameUtils.getExtension(file.getOriginalFilename())));
+//        try {
+//            Files.write(fileNamePath, file.getBytes());
+//            return new ResponseEntity<>(name, HttpStatus.CREATED);
+//        } catch (IOException ex) {
+//            return new ResponseEntity<>("Image is not uploaded", HttpStatus.BAD_REQUEST);
+//        }
+//    }
+//
+//    private void makeDirectoryIfNotExist(String imageDirectory) {
+//        File directory = new File(imageDirectory);
+//        if (!directory.exists()) {
+//            directory.mkdir();
+//        }
+//    }
+}
